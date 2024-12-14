612134e646315c426631ce9b51dfb84d7a4f3b2b
Menna <mennamohyza@gmail.com>
2024-12-14 23:10:14 +0200

    game 2

--- SUS.h
+++ SUS.h
@@ -6,11 +6,14 @@
 #include <iomanip>
 #include <cctype>
 using namespace std;
-
+static bool isRandom9 = false;
+Player<char>* globalPlayers_SUS[2];
 
 template <typename T>
 class SUS_Board:public Board<T> {
 public:
+    int player1Points = 0;
+    int player2Points = 0;
     SUS_Board (){
         this->rows = this->columns = 3;
         this->board = new char*[this->rows];
@@ -34,40 +37,87 @@ public:
             }
 
             return true;
+        }else{
+            if(!isRandom9){
+                cerr << "Invalid move! \n";
+            }
         }
         return false;
     }
     void display_board () override{
+        // Print the column indices
+        cout << "   ";
+        for (int j = 0; j < this->columns; j++) {
+            cout << setw(3) << j << "  "; // Column numbers at the top
+        }
+        cout << "\n";
+
+        // Print each row with its content
         for (int i = 0; i < this->rows; i++) {
-            cout << "\n| ";
+            cout << i << " |"; // Row index at the start
             for (int j = 0; j < this->columns; j++) {
-                cout << "(" << i << "," << j << ")";
-                cout << setw(2) << this->board[i][j] << " |";
+                char display_char = this->board[i][j] == 0 ? ' ' : this->board[i][j]; // Empty space for 0
+                cout << setw(3) << display_char << " |";
             }
-            cout << "\n-----------------------------";
+            cout << "\n    " << "________________" << "\n"; // Separator line
         }
         cout << endl;
     }
-    bool is_win() override{
+
+    int playerCounter() {
+        int player_count = 0;
+
         for (int i = 0; i < this->rows; i++) {
-            if ((this->board[i][0] == this->board[i][1] && this->board[i][1] == this->board[i][2] && this->board[i][0] != 0) ||
-                (this->board[0][i] == this->board[1][i] && this->board[1][i] == this->board[2][i] && this->board[0][i] != 0)) {
-                return true;
+            // Check row
+            if (this->board[i][0] == 'S' && this->board[i][1] == 'U' && this->board[i][2] == 'S') {
+                this->n_moves % 2 == 0 ? player1Points++ : player2Points++;
+                player_count++;
+            }
+            // Check column
+            if (this->board[0][i] == 'S' && this->board[1][i] == 'U' && this->board[2][i] == 'S') {
+                this->n_moves % 2 == 0 ? player1Points++ : player2Points++;
+                player_count++;
             }
         }
 
         // Check diagonals
-        if ((this->board[0][0] == this->board[1][1] && this->board[1][1] == this->board[2][2] && this->board[0][0] != 0) ||
-            (this->board[0][2] == this->board[1][1] && this->board[1][1] == this->board[2][0] && this->board[0][2] != 0)) {
-            return true;
+        if ((this->board[0][0] == 'S' && this->board[1][1] == 'U' && this->board[2][2] == 'S') ||
+            (this->board[0][2] == 'S' && this->board[1][1] == 'U' && this->board[2][0] == 'S')) {
+            this->n_moves % 2 == 0 ? player1Points++ : player2Points++;
+            player_count++;
+        }
+        return player_count;
+    }
+
+
+    bool is_win() override {
+
+        if(this-> n_moves == 9) {
+            int count_p1 = playerCounter('S');
+            int count_p2 = playerCounter('U');
+
+            if (count_p1 > count_p2 ) {
+                // Access and update player names
+                *globalPlayers_ttt[1] = *globalPlayers_ttt[0];  // This copies the entire player object
+                return true;
+            }
+            else if(count_p1 < count_p2){
+                return true;
+            }
+
         }
 
         return false;
     }
-    bool is_draw() override{
+
+
+    bool is_draw() override {
+        // A draw happens when the board is full and no player has won
         return (this->n_moves == 9 && !is_win());
     }
-    bool game_is_over()override{
+
+    bool game_is_over() override {
+        // The game ends if there's a win or a draw
         return is_win() || is_draw();
     }
 
@@ -88,7 +138,6 @@ public:
     void getmove(int &x, int &y) ;
 };
 
-// Constructor for X_O_Player
 template <typename T>
 SUS_Player<T>::SUS_Player(string name, T symbol) : Player<T>(name, symbol) {}
 
@@ -98,7 +147,6 @@ void SUS_Player<T>::getmove(int& x, int& y) {
     cin >> x >> y;
 }
 
-// Constructor for X_O_Random_Player
 template <typename T>
 SUS_Random_Player<T>::SUS_Random_Player(T symbol) : RandomPlayer<T>(symbol) {
     this->dimension = 3;
@@ -110,6 +158,7 @@ template <typename T>
 void SUS_Random_Player<T>::getmove(int& x, int& y) {
     x = rand() % this->dimension;  // Random number between 0 and 2
     y = rand() % this->dimension;
+    isRandom9 = true;
 }
 
 #endif //BOARD_GAMES_SUS_H
